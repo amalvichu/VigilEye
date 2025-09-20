e9c92fe4d84c5606f265df5db4dad9fcf34a183c
amalvichu <amalvpillai5@example.com>
2025-09-20 09:35:57 +0530

    added new features with gradients

--- api/models.py
+++ api/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class Device(models.Model):
@@ -16,3 +17,25 @@ class Alert(models.Model):
 
     def __str__(self):
         return f"Alert for {self.device.kindred_id} (Score: {self.score})"
+
+
+class Vigileye_user(AbstractUser):
+    Name = models.CharField(max_length=200, blank=True)
+    Phone_no = models.CharField(max_length=20, unique=True)
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='vigileye_users',  # Corrected related_name
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        verbose_name='groups',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='vigileye_users_permissions',  # Corrected related_name
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
+    def __str__(self):
+        return self.username
